840abd830c404934b2c2aa322c55a5cba310acb6
Hoa Nguyen <hoann.2016@gmail.com>
2024-06-02 16:41:46 +0700

    small fix

--- SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
@@ -29,7 +29,7 @@ namespace Post.Cmd.Api.Controllers
                 await _commandDispatcher.SendAsync(command);
                 return StatusCode(StatusCodes.Status201Created, new NewPostResponse
                 {
-                               
+                    Id = id,   
                     Message = $"New post creation request with message {command.Message} completed successfully.",
                 });
             }
